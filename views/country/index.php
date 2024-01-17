13c4e0c5adfde1923833eebe65b3cde279c85a55
ndys <hendy.sg02@gmail.com>
2024-01-17 15:31:19 +0700

    Generating code with Gii

--- views/country/index.php
+++ views/country/index.php
@@ -1,16 +1,45 @@
 <?php
+
+use app\models\Country;
 use yii\helpers\Html;
-use yii\widgets\LinkPager;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+
+/** @var yii\web\View $this */
+/** @var app\models\CountrySearch $searchModel */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+
+$this->title = 'Countries';
+$this->params['breadcrumbs'][] = $this->title;
 ?>
+<div class="country-index">
 
-<h1>Countries</h1>
-<ul>
-    <?php foreach ($countries as $country): ?>
-        <li>
-            <?= Html::encode("{$country->code} ({$country->name})") ?>:
-            <?= $country->population ?>
-        </li>
-    <?php endforeach; ?>
-</ul>
+    <h1><?= Html::encode($this->title) ?></h1>
 
-<?= LinkPager::widget(['pagination' => $pagination]) ?>
+    <p>
+        <?= Html::a('Create Country', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'code',
+            'name',
+            'population',
+            [
+                'class' => ActionColumn::className(),
+                'urlCreator' => function ($action, Country $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'code' => $model->code]);
+                 }
+            ],
+        ],
+    ]); ?>
+
+
+</div>
